5befa37ebc3121ad4299ccd2fee7174671542691
Tronikel <contactdonatas@gmail.com>
2024-10-11 08:31:08 +0300

    changes are scoped to buffer

--- lua/debdiag/init.lua
+++ lua/debdiag/init.lua
@@ -15,21 +15,22 @@ function M.setup(config)
     config = config or {}
     M.config = vim.tbl_deep_extend("force", M.config, config)
 
-    local diagnostics_show = utils.debounce(function()
-        vim.diagnostic.enable(true)
+    local diagnostics_show = utils.debounce(function(buf)
+        vim.diagnostic.enable(true, { bufnr = buf })
     end, M.config.ms)
 
     vim.api.nvim_create_autocmd(M.config.autocmd, {
         callback = function()
-            vim.diagnostic.enable(false, { bufnr = 0 })
-            diagnostics_show()
+            local buf = vim.api.nvim_get_current_buf()
+            vim.diagnostic.enable(false, { bufnr = buf })
+            diagnostics_show(buf)
         end,
     })
 
     if M.config.enable_leave_insert then
         vim.api.nvim_create_autocmd("InsertLeave", {
             callback = function()
-                vim.diagnostic.enable(true)
+                vim.diagnostic.enable(true, { bufnr = 0 })
             end,
         })
     end
