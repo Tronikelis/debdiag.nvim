1c3720f2a8604ef44dd8c01b9a2eaee7c94ca918
Tronikel <contactdonatas@gmail.com>
2024-10-14 18:21:40 +0300

    perf: simpler debounce fn

--- lua/debdiag/utils.lua
+++ lua/debdiag/utils.lua
@@ -2,36 +2,15 @@ local M = {}
 
 ---@param callback fun()
 ---@param ms integer
-function M.setTimeout(callback, ms)
-    callback = vim.schedule_wrap(callback)
-
-    local timer = vim.uv.new_timer()
-    timer:start(ms, 0, function()
-        timer:stop()
-        timer:close()
-        callback()
-    end)
-
-    return timer
-end
-
----@param callback fun()
----@param ms integer
 function M.debounce(callback, ms)
-    callback = vim.schedule_wrap(callback)
-    local timer = nil
+    local timer = vim.uv.new_timer()
 
     return function(...)
         local args = { ... }
 
-        if timer then
-            pcall(timer.stop, timer)
-            pcall(timer.close, timer)
-        end
-
-        timer = M.setTimeout(function()
-            callback(unpack(args))
-        end, ms)
+        timer:start(ms, 0, function()
+            vim.schedule_wrap(callback)(unpack(args))
+        end)
     end
 end
 
